2af91f47289d8701c40e431b3ab1c131ca826e66
phuongtran7 <phuong.tran@jvncomm.com>
2020-02-29 14:27:59 -0500

    Allow multiple files input and specifying number of member and generation.

--- Accelerator/Accelerator.cpp
+++ Accelerator/Accelerator.cpp
@@ -2,12 +2,59 @@
 #include "fmt/format.h"
 #include "Population.h"
 #include <chrono>
+#include <filesystem>
+
+std::string get_input() {
+	std::vector<std::string> files{};
+	auto current_path = std::filesystem::current_path();
+	for (const auto& entry : std::filesystem::directory_iterator(current_path)) {
+		// Look for ".tsp" file in the directory
+		auto ext = entry.path().extension().generic_string();
+		if (ext == ".tsp") {
+			files.push_back(entry.path().filename().string());
+		}
+	}
+
+	if (!files.empty()) {
+		if (files.size() > 1) {
+			fmt::print("There are multiple \".tsp\" files. Please choose one: \n");
+			for (auto i = 0; i < files.size(); i++) {
+				fmt::print("{} - {}\n", i, files.at(i));
+			}
+
+			int choice;
+			std::cin >> choice;
+			std::cin.get();
+			return files.at(choice);
+		}
+		else {
+			return files.at(0);
+		}
+	}
+	else {
+		fmt::print("Cannot find \".tsp\" files. Shutting down.\n");
+		return "";
+	}
+}
 
 int main()
 {
-	Population Nora("dj38.tsp", 100);
+	auto file_name = get_input();
+	if (file_name.empty()) {
+		return 1;
+	}
+
+	size_t pop_size, gen_size;
+	fmt::print("Please specify how large the population: ");
+	std::cin >> pop_size;
+	std::cin.get();
+	fmt::print("Please specify how many generation: ");
+	std::cin >> gen_size;
+	std::cin.get();
+
+	Population Nora(file_name, pop_size);
 	auto t1 = std::chrono::high_resolution_clock::now();
-	for (auto i = 0; i < 10000; i++) {
+	for (auto i = 0; i < gen_size; i++) {
 		Nora.report_data();
 		Nora.start_next_generation();
 	}
